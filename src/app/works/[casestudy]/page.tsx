5f960b879685a22326792381efff190af1f3b0ff
Pariola <kehindephilip15@gmail.com>
2025-02-25 17:59:24 +0100

    cleanup

--- src/app/works/[casestudy]/page.tsx
+++ src/app/works/[casestudy]/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+
+import PageWrapper from '@/components/common/PageWrapper';
+import { gsap } from 'gsap';
+import { Flip } from 'gsap/dist/Flip';
+import { useState } from 'react';
+
+gsap.registerPlugin(Flip);
+
 export default function CaseStudyPage() {
-  return <div>page</div>;
+  const [bgColor, setBgColor] = useState('#ffffff');
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isVertical, setIsVertical] = useState<Boolean>(true);
+
+  return (
+    <>
+      <PageWrapper
+        theme="light"
+        className={`pageWorks`}
+        style={{ backgroundColor: bgColor }}
+        lenis={{}}
+      ></PageWrapper>
+    </>
+  );
 }
